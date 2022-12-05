7deee80b2eede93aa930fdcc5c474befc102d233
dabico <dabico@usi.ch>
2022-12-05 14:32:43 +0100

    Added convenience methods for parsing

--- src/main/java/ai/serenade/treesitter/Parser.java
+++ src/main/java/ai/serenade/treesitter/Parser.java
@@ -1,7 +1,11 @@
 package ai.serenade.treesitter;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Parsers are stateful objects that can be assigned a language and used to produce a
@@ -36,6 +40,28 @@ public class Parser implements AutoCloseable {
   public Tree parseString(String source) throws UnsupportedEncodingException {
     byte[] bytes = source.getBytes(StandardCharsets.UTF_16LE);
     return new Tree(TreeSitter.parserParseBytes(pointer, bytes, bytes.length));
+  }
+
+  /**
+   * Use the parser to parse some source code found in a file at the specified path.
+   *
+   * @param path The path of the file to be parsed.
+   * @return A tree-sitter Tree matching the provided source.
+   */
+  public Tree parseFile(Path path) throws IOException {
+    String source = Files.readString(path);
+    return parseString(source);
+  }
+
+  /**
+   * Use the parser to parse some source code found in a file represented by the reference.
+   *
+   * @param file The reference to the file which will be parsed.
+   * @return A tree-sitter Tree matching the provided source.
+   */
+  public Tree parseFile(File file) throws IOException {
+    Path path = file.toPath();
+    return parseFile(path);
   }
 
   /**
