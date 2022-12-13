e06aab02ae41258cc0eca4ea2df0a98aae2e7877
dabico <dabico@usi.ch>
2022-12-13 14:08:16 +0100

    Added tests for the newly introduced APIs

--- src/test/java/ai/serenade/treesitter/NodeTest.java
+++ src/test/java/ai/serenade/treesitter/NodeTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 
 class NodeTest extends TestBase {
 
+  private static final String source = "def foo(bar, baz):\n  print(bar)\n  print(baz)";
+
   @Test
-  void testGetChildren() throws UnsupportedEncodingException {
+  void testGetChild() throws UnsupportedEncodingException {
     try (Parser parser = new Parser()) {
       parser.setLanguage(Language.PYTHON);
-      try (Tree tree = parser.parseString("def foo(bar, baz):\n  print(bar)\n  print(baz)")) {
+      try (Tree tree = parser.parseString(source)) {
         Node root = tree.getRootNode();
         Assertions.assertEquals(1, root.getChildCount());
         Assertions.assertEquals("module", root.getType());
@@ -21,6 +23,117 @@ class NodeTest extends TestBase {
         Node function = root.getChild(0);
         Assertions.assertEquals("function_definition", function.getType());
         Assertions.assertEquals(5, function.getChildCount());
+      }
+    }
+  }
+
+  @Test
+  void testGetChildByFieldName() throws UnsupportedEncodingException {
+    try (Parser parser = new Parser()) {
+      parser.setLanguage(Language.PYTHON);
+      try (Tree tree = parser.parseString(source)) {
+        Node root = tree.getRootNode();
+        Node function = root.getChild(0);
+        Node identifier = function.getChild(1);
+        Assertions.assertEquals(identifier, function.getChildByFieldName("name"));
+      }
+    }
+  }
+
+  @Test
+  void testGetParent() throws UnsupportedEncodingException {
+    try (Parser parser = new Parser()) {
+      parser.setLanguage(Language.PYTHON);
+      try (Tree tree = parser.parseString(source)) {
+        Node root = tree.getRootNode();
+        Assertions.assertEquals(root, root.getChild(0).getParent());
+      }
+    }
+  }
+
+  @Test
+  void testGetNextSibling() throws UnsupportedEncodingException {
+    try (Parser parser = new Parser()) {
+      parser.setLanguage(Language.PYTHON);
+      try (Tree tree = parser.parseString(source)) {
+        Node root = tree.getRootNode();
+        Node function = root.getChild(0);
+        Node def = function.getChild(0);
+        Node identifier = function.getChild(1);
+        Assertions.assertEquals(identifier, def.getNextSibling());
+      }
+    }
+  }
+
+  @Test
+  void testGetPrevSibling() throws UnsupportedEncodingException {
+    try (Parser parser = new Parser()) {
+      parser.setLanguage(Language.PYTHON);
+      try (Tree tree = parser.parseString(source)) {
+        Node root = tree.getRootNode();
+        Node function = root.getChild(0);
+        Node def = function.getChild(0);
+        Node identifier = function.getChild(1);
+        Assertions.assertEquals(def, identifier.getPrevSibling());
+      }
+    }
+  }
+
+  @Test
+  void testHasError() throws UnsupportedEncodingException {
+    try (Parser parser = new Parser()) {
+      parser.setLanguage(Language.PYTHON);
+      try (Tree tree = parser.parseString("def foo(bar, baz):\n  print(bar.)")) {
+        Node root = tree.getRootNode();
+        Node function = root.getChild(0);
+        Node def = function.getChild(0);
+        Assertions.assertTrue(root.hasError());
+        Assertions.assertTrue(function.hasError());
+        Assertions.assertFalse(def.hasError());
+      }
+    }
+  }
+
+  @Test
+  void testIsExtra() throws UnsupportedEncodingException {
+    try (Parser parser = new Parser()) {
+      parser.setLanguage(Language.PYTHON);
+      try (Tree tree = parser.parseString("# this is just a comment")) {
+        Node root = tree.getRootNode();
+        Node comment = root.getChild(0);
+        Assertions.assertFalse(root.isExtra());
+        Assertions.assertTrue(comment.isExtra());
+      }
+    }
+  }
+
+  @Test
+  void testIsMissing() throws UnsupportedEncodingException {
+    try (Parser parser = new Parser()) {
+      parser.setLanguage(Language.JAVA);
+      try (Tree tree = parser.parseString("class C { public static final int i = 6 }")) {
+        Node root = tree.getRootNode();
+        Assertions.assertFalse(root.isMissing());
+        Assertions.assertFalse(root.getChild(0).isMissing());
+        Assertions.assertFalse(root.getChild(0).getChild(2).isMissing());
+        Assertions.assertFalse(root.getChild(0).getChild(2).getChild(1).isMissing());
+        Assertions.assertFalse(root.getChild(0).getChild(2).getChild(1).getChild(2).isMissing());
+        Assertions.assertTrue(root.getChild(0).getChild(2).getChild(1).getChild(3).isMissing());
+      }
+    }
+  }
+
+  @Test
+  void testIsNamed() throws UnsupportedEncodingException {
+    try (Parser parser = new Parser()) {
+      parser.setLanguage(Language.PYTHON);
+      try (Tree tree = parser.parseString(source)) {
+        Node root = tree.getRootNode();
+        Node function = root.getChild(0);
+        Node def = function.getChild(0);
+        Node identifier = function.getChild(1);
+        Assertions.assertFalse(def.isNamed());
+        Assertions.assertTrue(identifier.isNamed());
       }
     }
   }
