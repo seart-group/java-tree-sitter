2673c64c13d6fdf46f69c8a700d4ab35dc9b5e25
dabico <dabico@usi.ch>
2023-04-28 10:46:38 +0200

    Added test case for printing syntax trees with positional offset

--- src/test/java/usi/si/seart/treesitter/SyntaxTreePrinterTest.java
+++ src/test/java/usi/si/seart/treesitter/SyntaxTreePrinterTest.java
@@ -89,4 +89,47 @@ class SyntaxTreePrinterTest extends TestBase {
                 "          string_literal [5:27] - [5:42]\n";
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    @SneakyThrows(UnsupportedEncodingException.class)
+    void testPrintSubtreeWithOffset() {
+        SyntaxTreePrinter printer;
+        @Cleanup Parser parser = new Parser(Language.JAVA);
+        @Cleanup Tree tree = parser.parseString(
+                "package ch.usi.si;\n" +
+                "\n" +
+                "public class Main {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        //line comment\n" +
+                "        System.out.println(\"Hello, World!\");\n" +
+                "    }\n" +
+                "}"
+        );
+        Node root = tree.getRootNode();
+        Node method = root.getChild(1).getChildByFieldName("body").getChild(1);
+        printer = new SyntaxTreePrinter(method, new Point(-1, -2));
+        String actual = printer.printSubtree();
+        String expected =
+                "method_declaration [2:2] - [5:3]\n" +
+                "  modifiers [2:2] - [2:15]\n" +
+                "  type: void_type [2:16] - [2:20]\n" +
+                "  name: identifier [2:21] - [2:25]\n" +
+                "  parameters: formal_parameters [2:25] - [2:40]\n" +
+                "    formal_parameter [2:26] - [2:39]\n" +
+                "      type: array_type [2:26] - [2:34]\n" +
+                "        element: type_identifier [2:26] - [2:32]\n" +
+                "        dimensions: dimensions [2:32] - [2:34]\n" +
+                "      name: identifier [2:35] - [2:39]\n" +
+                "  body: block [2:41] - [5:3]\n" +
+                "    line_comment [3:6] - [3:20]\n" +
+                "    expression_statement [4:6] - [4:42]\n" +
+                "      method_invocation [4:6] - [4:41]\n" +
+                "        object: field_access [4:6] - [4:16]\n" +
+                "          object: identifier [4:6] - [4:12]\n" +
+                "          field: identifier [4:13] - [4:16]\n" +
+                "        name: identifier [4:17] - [4:24]\n" +
+                "        arguments: argument_list [4:24] - [4:41]\n" +
+                "          string_literal [4:25] - [4:40]\n";
+        Assertions.assertEquals(expected, actual);
+    }
 }
