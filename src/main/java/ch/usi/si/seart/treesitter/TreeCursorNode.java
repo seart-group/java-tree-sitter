d3811856ea8d8d42c9fd9ecfd2c69572b1005f33
dabico <dabico@usi.ch>
2023-09-02 14:53:26 +0200

    `TreeCursorNode` is now once again immutable

--- src/main/java/ch/usi/si/seart/treesitter/TreeCursorNode.java
+++ src/main/java/ch/usi/si/seart/treesitter/TreeCursorNode.java
@@ -4,11 +4,9 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Generated;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
-@Setter(value = AccessLevel.PACKAGE)
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TreeCursorNode {
