b79172960bf4e5ab87c8bdd3bfef0ca398f6f3c0
dabico <dabico@usi.ch>
2023-09-03 13:17:48 +0200

    Added documentation to `ParsingException`

--- src/main/java/ch/usi/si/seart/treesitter/exception/ParsingException.java
+++ src/main/java/ch/usi/si/seart/treesitter/exception/ParsingException.java
@@ -2,6 +2,9 @@ package ch.usi.si.seart.treesitter.exception;
 
 import lombok.experimental.StandardException;
 
+/**
+ * Thrown when there is an error during parsing.
+ */
 @StandardException
 public class ParsingException extends TreeSitterException {
 }
