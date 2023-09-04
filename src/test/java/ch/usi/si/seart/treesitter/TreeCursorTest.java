04ca989b35cd166a5c746e2eeae8026b557dabe1
dabico <dabico@usi.ch>
2023-09-04 12:01:49 +0200

    Fixing indentation in `TreeCursorTest`

--- src/test/java/ch/usi/si/seart/treesitter/TreeCursorTest.java
+++ src/test/java/ch/usi/si/seart/treesitter/TreeCursorTest.java
@@ -13,74 +13,74 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 class TreeCursorTest extends TestBase {
 
-  private static Parser parser;
-  private static Tree tree;
+    private static Parser parser;
+    private static Tree tree;
 
-  private TreeCursor cursor;
+    private TreeCursor cursor;
 
-  @BeforeAll
-  @SneakyThrows(UnsupportedEncodingException.class)
-  static void beforeAll() {
-    parser = new Parser(Language.PYTHON);
-    tree = parser.parseString("def foo(bar, baz):\n  print(bar)\n  print(baz)");
-  }
+    @BeforeAll
+    @SneakyThrows(UnsupportedEncodingException.class)
+    static void beforeAll() {
+        parser = new Parser(Language.PYTHON);
+        tree = parser.parseString("def foo(bar, baz):\n  print(bar)\n  print(baz)");
+    }
 
-  @BeforeEach
-  void setUp() {
-    cursor = tree.getRootNode().walk();
-  }
+    @BeforeEach
+    void setUp() {
+        cursor = tree.getRootNode().walk();
+    }
 
-  @AfterEach
-  void tearDown() {
-    cursor.close();
-  }
+    @AfterEach
+    void tearDown() {
+        cursor.close();
+    }
 
-  @AfterAll
-  static void afterAll() {
-    tree.close();
-    parser.close();
-  }
+    @AfterAll
+    static void afterAll() {
+        tree.close();
+        parser.close();
+    }
 
-  @Test
-  void testWalk() {
-    Assertions.assertEquals("module", cursor.getCurrentTreeCursorNode().getType());
-    Assertions.assertEquals("module", cursor.getCurrentNode().getType());
-    Assertions.assertTrue(cursor.gotoFirstChild());
-    Assertions.assertEquals("function_definition", cursor.getCurrentNode().getType());
-    Assertions.assertTrue(cursor.gotoFirstChild());
+    @Test
+    void testWalk() {
+        Assertions.assertEquals("module", cursor.getCurrentTreeCursorNode().getType());
+        Assertions.assertEquals("module", cursor.getCurrentNode().getType());
+        Assertions.assertTrue(cursor.gotoFirstChild());
+        Assertions.assertEquals("function_definition", cursor.getCurrentNode().getType());
+        Assertions.assertTrue(cursor.gotoFirstChild());
 
-    Assertions.assertEquals("def", cursor.getCurrentNode().getType());
-    Assertions.assertTrue(cursor.gotoNextSibling());
-    Assertions.assertEquals("identifier", cursor.getCurrentNode().getType());
-    Assertions.assertTrue(cursor.gotoNextSibling());
-    Assertions.assertEquals("parameters", cursor.getCurrentNode().getType());
-    Assertions.assertTrue(cursor.gotoNextSibling());
-    Assertions.assertEquals(":", cursor.getCurrentNode().getType());
-    Assertions.assertTrue(cursor.gotoNextSibling());
-    Assertions.assertEquals("block", cursor.getCurrentNode().getType());
-    Assertions.assertEquals("body", cursor.getCurrentFieldName());
-    Assertions.assertFalse(cursor.gotoNextSibling());
+        Assertions.assertEquals("def", cursor.getCurrentNode().getType());
+        Assertions.assertTrue(cursor.gotoNextSibling());
+        Assertions.assertEquals("identifier", cursor.getCurrentNode().getType());
+        Assertions.assertTrue(cursor.gotoNextSibling());
+        Assertions.assertEquals("parameters", cursor.getCurrentNode().getType());
+        Assertions.assertTrue(cursor.gotoNextSibling());
+        Assertions.assertEquals(":", cursor.getCurrentNode().getType());
+        Assertions.assertTrue(cursor.gotoNextSibling());
+        Assertions.assertEquals("block", cursor.getCurrentNode().getType());
+        Assertions.assertEquals("body", cursor.getCurrentFieldName());
+        Assertions.assertFalse(cursor.gotoNextSibling());
 
-    Assertions.assertTrue(cursor.gotoParent());
-    Assertions.assertEquals("function_definition", cursor.getCurrentNode().getType());
-    Assertions.assertTrue(cursor.gotoFirstChild());
-  }
+        Assertions.assertTrue(cursor.gotoParent());
+        Assertions.assertEquals("function_definition", cursor.getCurrentNode().getType());
+        Assertions.assertTrue(cursor.gotoFirstChild());
+    }
 
-  @Test
-  void testPreorderTraversal() {
-    AtomicInteger count = new AtomicInteger();
-    cursor.preorderTraversal(node -> {
-      if (node.isNamed())
-        count.incrementAndGet();
-    });
-    Assertions.assertEquals(17, count.get());
-  }
+    @Test
+    void testPreorderTraversal() {
+        AtomicInteger count = new AtomicInteger();
+        cursor.preorderTraversal(node -> {
+            if (node.isNamed())
+                count.incrementAndGet();
+        });
+        Assertions.assertEquals(17, count.get());
+    }
 
-  @Test
-  @SuppressWarnings("resource")
-  void testWalkException() {
-    Node nullNode = new Node();
-    Assertions.assertThrows(NullPointerException.class, () -> new TreeCursor(null));
-    Assertions.assertThrows(IllegalArgumentException.class, nullNode::walk);
-  }
+    @Test
+    @SuppressWarnings("resource")
+    void testWalkException() {
+        Node nullNode = new Node();
+        Assertions.assertThrows(NullPointerException.class, () -> new TreeCursor(null));
+        Assertions.assertThrows(IllegalArgumentException.class, nullNode::walk);
+    }
 }
