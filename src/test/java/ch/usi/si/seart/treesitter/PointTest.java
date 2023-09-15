b36ab5be78cf8d160029ee2a549164190207623a
dabico <dabico@usi.ch>
2023-09-15 09:31:48 +0200

    Parametrizing the tests for `Point`

--- src/test/java/ch/usi/si/seart/treesitter/PointTest.java
+++ src/test/java/ch/usi/si/seart/treesitter/PointTest.java
@@ -2,17 +2,33 @@ package ch.usi.si.seart.treesitter;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class PointTest {
 
     @Test
     void testIsOrigin() {
         Assertions.assertTrue(new Point(0, 0).isOrigin());
-        Assertions.assertFalse(new Point(1, 0).isOrigin());
-        Assertions.assertFalse(new Point(0, 1).isOrigin());
-        Assertions.assertFalse(new Point(1, 1).isOrigin());
-        Assertions.assertFalse(new Point(-1, 0).isOrigin());
-        Assertions.assertFalse(new Point(0, -1).isOrigin());
-        Assertions.assertFalse(new Point(-1, -1).isOrigin());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNonOriginPoints")
+    void testIsNotOrigin(Point point) {
+        Assertions.assertFalse(point.isOrigin());
+    }
+
+    public static Stream<Arguments> provideNonOriginPoints() {
+        return Stream.of(
+                Arguments.of(new Point(1, 0)),
+                Arguments.of(new Point(0, 1)),
+                Arguments.of(new Point(1, 1)),
+                Arguments.of(new Point(-1, 0)),
+                Arguments.of(new Point(0, -1)),
+                Arguments.of(new Point(-1, -1))
+        );
     }
 }
