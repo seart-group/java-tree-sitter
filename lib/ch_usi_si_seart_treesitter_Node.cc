2dfa88b74510b7fa54ad43077e9e75d9d0cda158
dabico <dabico@usi.ch>
2023-09-20 11:43:04 +0200

    The `getType` method now returns `null` for a "null" `Node`

--- lib/ch_usi_si_seart_treesitter_Node.cc
+++ lib/ch_usi_si_seart_treesitter_Node.cc
@@ -196,6 +196,7 @@ JNIEXPORT jobject JNICALL Java_ch_usi_si_seart_treesitter_Node_getStartPoint(
 JNIEXPORT jstring JNICALL Java_ch_usi_si_seart_treesitter_Node_getType(
   JNIEnv* env, jobject thisObject) {
   TSNode node = __unmarshalNode(env, thisObject);
+  if (ts_node_is_null(node)) return NULL;
   const char* type = ts_node_type(node);
   jstring result = env->NewStringUTF(type);
   return result;
