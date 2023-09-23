131b3b39c04757c52e2f54c9ec91b8cbd2a91a71
dabico <dabico@usi.ch>
2023-09-23 18:55:23 +0200

    Added "treeless" `Node` to `TestBase` for reuse in tests

--- src/test/java/ch/usi/si/seart/treesitter/TestBase.java
+++ src/test/java/ch/usi/si/seart/treesitter/TestBase.java
@@ -3,6 +3,7 @@ package ch.usi.si.seart.treesitter;
 public abstract class TestBase {
 
     protected final Node empty = new Node();
+    protected final Node treeless = new Node(1, 1, 1, 1, 1L, null);
     protected final Point _0_0_ = new Point(0, 0);
     protected final Point _0_1_ = new Point(0, 1);
     protected final Point _1_0_ = new Point(1, 0);
