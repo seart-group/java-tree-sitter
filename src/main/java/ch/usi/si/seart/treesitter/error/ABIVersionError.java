95fd1c1f00f87a5cdbe357ac0a1478b42e2e60ad
dabico <dabico@usi.ch>
2023-09-26 15:58:39 +0200

    Adding a shorthand constructor to `ABIVersionError`

--- src/main/java/ch/usi/si/seart/treesitter/error/ABIVersionError.java
+++ src/main/java/ch/usi/si/seart/treesitter/error/ABIVersionError.java
@@ -13,4 +13,10 @@ import lombok.experimental.StandardException;
  */
 @StandardException
 public class ABIVersionError extends LinkageError {
+
+    private static final String TEMPLATE = "Incompatible language version: %d";
+
+    public ABIVersionError(int version) {
+        super(String.format(TEMPLATE, version));
+    }
 }
