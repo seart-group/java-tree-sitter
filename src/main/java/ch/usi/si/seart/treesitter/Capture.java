641f9f6ea88111f5be1a9077ae52fa99bdfa45e0
dabico <dabico@usi.ch>
2023-09-29 10:21:33 +0200

    Added class-level documentation to `Capture` and `Pattern`

--- src/main/java/ch/usi/si/seart/treesitter/Capture.java
+++ src/main/java/ch/usi/si/seart/treesitter/Capture.java
@@ -5,6 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
+/**
+ * Represents the named capture of a {@link Query}.
+ * Captures are used to extract information from
+ * syntax trees when a query match occurs.
+ * Each instance consists of an ordinal representing
+ * its order of appearance within the query,
+ * as well as the name used to refer to the
+ * captured node in the results.
+ *
+ * @since 1.7.0
+ * @author Ozren Dabić
+ * @see Pattern
+ * @see Query
+ */
 @Getter
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
--- src/main/java/ch/usi/si/seart/treesitter/Pattern.java
+++ src/main/java/ch/usi/si/seart/treesitter/Pattern.java
@@ -5,6 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
+/**
+ * Represents a single symbolic expression (s-expression) pattern of a {@link Query}.
+ * Said pattern is a structured representation of a syntax tree fragment,
+ * which can be used to query {@link Node} subtrees. Each instance consists
+ * of an ordinal representing its order of appearance within the query,
+ * as well as the actual pattern value.
+ *
+ * @since 1.7.0
+ * @author Ozren Dabić
+ * @see Capture
+ * @see Query
+ */
 @Getter
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
