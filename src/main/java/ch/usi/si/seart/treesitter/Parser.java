697c38180cb665a141f804b6f8a1c05c119fac18
dabico <dabico@usi.ch>
2023-10-31 12:02:47 +0100

    Removed `toString` method of `Parser`

--- src/main/java/ch/usi/si/seart/treesitter/Parser.java
+++ src/main/java/ch/usi/si/seart/treesitter/Parser.java
@@ -3,7 +3,6 @@ package ch.usi.si.seart.treesitter;
 import ch.usi.si.seart.treesitter.exception.parser.IncompatibleLanguageException;
 import ch.usi.si.seart.treesitter.exception.parser.ParsingException;
 import lombok.AccessLevel;
-import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -274,10 +273,4 @@ public class Parser extends External {
     }
 
     private native Tree parse(String source, byte[] bytes, int length, Tree oldTree);
-
-    @Override
-    @Generated
-    public String toString() {
-        return String.format("Parser(language: %s)", language);
-    }
 }
