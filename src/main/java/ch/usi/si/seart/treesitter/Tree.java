a985f4f4a9cdef75010856ada731aac02029dcd1
dabico <dabico@usi.ch>
2023-11-08 13:05:15 +0100

    Shorten @link in `Tree`

--- src/main/java/ch/usi/si/seart/treesitter/Tree.java
+++ src/main/java/ch/usi/si/seart/treesitter/Tree.java
@@ -12,7 +12,7 @@ import java.util.Iterator;
 
 /**
  * A Tree represents the syntax tree of an entire source code file.
- * It contains {@link Node Node} instances that indicate the structure of the source code.
+ * It contains {@link Node} instances that indicate the structure of the source code.
  *
  * @since 1.0.0
  * @author Tommy MacWilliam
