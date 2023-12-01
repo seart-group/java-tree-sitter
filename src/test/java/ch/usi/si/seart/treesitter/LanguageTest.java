2fd0bd2564a49d53dbef14e977d47efd1f5a16cf
dabico <dabico@usi.ch>
2023-12-01 16:08:59 +0100

    Adding additional test case for `Language#associatedWith`

--- src/test/java/ch/usi/si/seart/treesitter/LanguageTest.java
+++ src/test/java/ch/usi/si/seart/treesitter/LanguageTest.java
@@ -60,6 +60,7 @@ class LanguageTest extends TestBase {
             return Stream.of(
                     Arguments.of("requirements.txt", List.of()),
                     Arguments.of(".py", List.of(Language.PYTHON)),
+                    Arguments.of(".gitattributes", List.of(Language.GITATTRIBUTES)),
                     Arguments.of("__init__.py", List.of(Language.PYTHON)),
                     Arguments.of("Main.java", List.of(Language.JAVA)),
                     Arguments.of("example.h", List.of(
