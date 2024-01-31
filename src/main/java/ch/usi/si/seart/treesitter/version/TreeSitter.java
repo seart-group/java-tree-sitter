20422392d61aeeff535ed63fa8f8db8d7b4e0054
dabico <dabico@usi.ch>
2024-01-31 16:16:25 +0100

    Add generated source file `TreeSitter`

---
+++ src/main/java/ch/usi/si/seart/treesitter/version/TreeSitter.java
@@ -0,0 +1,18 @@
+package ch.usi.si.seart.treesitter.version;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * Contains version information pertaining to the {@code tree-sitter} API.
+ *
+ * @author Ozren Dabić
+ * @since 1.11.0
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class TreeSitter {
+
+    public static final String SHA = "98be227227af10cc7a269cb3ffb23686c0610b17";
+
+    public static final String TAG = "v0.20.9";
+}
