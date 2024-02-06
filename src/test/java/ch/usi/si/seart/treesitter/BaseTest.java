fa01e2e795817d6dc4863d49103282043b48d788
dabico <dabico@usi.ch>
2024-02-06 13:47:05 +0100

    Make all `BaseTest` members static

--- src/test/java/ch/usi/si/seart/treesitter/BaseTest.java
+++ src/test/java/ch/usi/si/seart/treesitter/BaseTest.java
@@ -8,13 +8,14 @@ public abstract class BaseTest {
 
     protected static final Language invalid;
 
-    protected final Node empty = new Node.Null();
-    protected final Node treeless = new Node(1, 1, 1, 1, 1L, null);
-    protected final Point _0_0_ = new Point(0, 0);
-    protected final Point _0_1_ = new Point(0, 1);
-    protected final Point _1_0_ = new Point(1, 0);
-    protected final Point _1_1_ = new Point(1, 1);
-    protected final Point _2_2_ = new Point(2, 2);
+    protected static final Node empty = new Node.Null();
+    protected static final Node treeless = new Node(1, 1, 1, 1, 1L, null);
+
+    protected static final Point _0_0_ = new Point(0, 0);
+    protected static final Point _0_1_ = new Point(0, 1);
+    protected static final Point _1_0_ = new Point(1, 0);
+    protected static final Point _1_1_ = new Point(1, 1);
+    protected static final Point _2_2_ = new Point(2, 2);
 
     static {
         LibraryLoader.load();
